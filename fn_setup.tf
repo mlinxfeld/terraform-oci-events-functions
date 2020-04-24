8d74ad2ef72d02f8dd8a08914bfb6267205d65eb
lukasz_feldman <lukasz.feldman@oracle.com>
2020-04-24 13:39:16 +0200

    Fixes on fn_setup.tf

--- fn_setup.tf
+++ fn_setup.tf
@@ -11,12 +11,22 @@ resource "null_resource" "FoggyKitchenFnSetup" {
   }
 
   provisioner "local-exec" {
+    command = "image=$(docker images | grep updatetagforbootvolumebackupfn | awk -F ' ' '{print $3}') ; docker tag $image ${var.ocir_docker_repository}/${var.ocir_namespace}/${var.ocir_repo_name}/updatetagforbootvolumebackupfn:0.0.1"
+    working_dir = "functions/UpdateTagForBootVolumeBackupFn"
+  }
+
+  provisioner "local-exec" {
     command = "docker push ${var.ocir_docker_repository}/${var.ocir_namespace}/${var.ocir_repo_name}/updatetagforbootvolumebackupfn:0.0.1"
     working_dir = "functions/UpdateTagForBootVolumeBackupFn"
   }
 
   provisioner "local-exec" {
     command = "fn build"
+    working_dir = "functions/UpdateTagForVolumeBackupFn"
+  }
+
+  provisioner "local-exec" {
+    command = "image=$(docker images | grep updatetagforvolumebackupfn | awk -F ' ' '{print $3}') ; docker tag $image ${var.ocir_docker_repository}/${var.ocir_namespace}/${var.ocir_repo_name}/updatetagforvolumebackupfn:0.0.1"
     working_dir = "functions/UpdateTagForVolumeBackupFn"
   }
 
